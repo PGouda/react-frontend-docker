1396f409944701d0c9a30bc1afc8ecc1f2622da5
Sharan <sharanubp@gmail.com>
2020-06-12 14:56:03 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sharn testing react!!
+          Bhuvan is awesome!!
         </p>
         <a
           className="App-link"
